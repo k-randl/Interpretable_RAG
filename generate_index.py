424d6e94da44c666ad84560cbec013ba79bb520f
Franco Maria Nardini <francomaria.nardini@isti.cnr.it>
2024-12-03 14:44:27 +0100

    temporary file for generting embeddings

---
+++ generate_index.py
@@ -0,0 +1,20 @@
+#%%
+import torch
+from transformers import AutoTokenizer, AutoModel
+tokenizer = AutoTokenizer.from_pretrained('facebook/dragon-plus-query-encoder')
+query_encoder = AutoModel.from_pretrained('facebook/dragon-plus-query-encoder')
+context_encoder = AutoModel.from_pretrained('facebook/dragon-plus-context-encoder')
+
+# We use msmarco query and passages as an example
+query =  "Where was Marie Curie born?"
+contexts = [
+    "Maria Sklodowska, later known as Marie Curie, was born on November 7, 1867.",
+    "Born in Paris on 15 May 1859, Pierre Curie was the son of Eugène Curie, a doctor of French Catholic origin from Alsace."
+]
+# Apply tokenizer
+query_input = tokenizer(query, return_tensors='pt')
+ctx_input = tokenizer(contexts, padding=True, truncation=True, return_tensors='pt')
+# Compute embeddings: take the last-layer hidden state of the [CLS] token
+query_emb = query_encoder(**query_input).last_hidden_state[:, 0, :]
+ctx_emb = context_encoder(**ctx_input).last_hidden_state[:, 0, :]
+# %%
