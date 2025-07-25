fe0a53fcfeaa8bda4cc72538789a45eb45887dc8
Korbinian Robert Randl <kora8563@dsv.su.se>
2025-07-25 14:41:24 +0200

    Updated demos.

---
+++ demos/explain_generation_llama.py
@@ -0,0 +1,64 @@
+# %%
+import os
+import sys
+sys.path.insert(0, "..")
+
+import torch
+from resources.generation import ExplainableAutoModelForGeneration
+from resources.plotting import plot_attribution_generator, higlight_attribution_generator
+
+from huggingface_hub import login
+from getpass import getpass
+
+# %%
+if os.path.exists('.huggingface.token'):
+    with open('.huggingface.token', 'r') as file:
+        login(token=file.read())
+
+else: login(token=getpass(prompt='Huggingface login  token: '))
+
+#%%
+MAX_SEQ_LEN = 1024
+MAX_GEN_LEN = 256
+# %% Load Pipeline:
+generator = ExplainableAutoModelForGeneration.from_pretrained(
+    pretrained_model_name_or_path='meta-llama/Llama-3.1-8B-Instruct',
+    device_map='auto',
+    torch_dtype=torch.bfloat16
+)
+
+# We use msmarco query and passages as an example
+query =  "Where was Marie Curie born?"
+contexts = [
+    "Maria Skłodowska, later known as Marie Curie, was born on November 7, 1867.",
+    "Born in Paris on 15 May 1859, Pierre Curie was the son of Eugène Curie, a doctor of French Catholic origin from Alsace.",
+    "Maria Skłodowska was born in Warsaw, in Congress Poland in the Russian Empire, as the fifth and youngest child of well-known teachers Bronisława, née Boguska, and Władysław Skłodowski.",
+    "While a French citizen, Marie Skłodowska Curie, who used both surnames, never lost her sense of Polish identity. She taught her daughters the Polish language and took them on visits to Poland.",
+    "Marie Curie founded the Curie Institute in Paris in 1920, and the Curie Institute in Warsaw in 1932.",
+]
+
+# %%
+output = generator.explain_generate(
+    query=query,
+    contexts=contexts,
+    max_new_tokens=MAX_GEN_LEN,
+    do_sample=False,
+    top_p=1,
+    num_beams=1,
+    max_samples=32,
+    conditional=True
+)
+output
+
+# %%
+plot_attribution_generator(generator, aggregation='token', normalize=True)
+higlight_attribution_generator(generator, token_processor=lambda s: s.replace('Ġ', ' ').strip('Ċ'))
+
+#%%
+plot_attribution_generator(generator, aggregation='bow', normalize=True)
+
+#%%
+plot_attribution_generator(generator, aggregation='nucleus', normalize=True)
+
+#%%
+plot_attribution_generator(generator, aggregation='sequence', normalize=True)
