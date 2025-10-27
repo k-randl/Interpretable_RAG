ab5b011e6e0bfede7d0fca88451a5fbbdff0e23c
Korbinian Robert Randl <kora8563@dsv.su.se>
2025-10-27 10:22:06 +0100

    Cleaned out old resources folder.

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@
 pip install -r ./requirements.txt
 
 # download bart score:
-wget -P ./resources/ https://raw.githubusercontent.com/neulab/BARTScore/main/bart_score.py
+mkdir ./src/longdocfactscore
+wget -P ./src/longdocfactscore/ https://raw.githubusercontent.com/neulab/BARTScore/main/bart_score.py
 
 # download ldfacts score:
-wget -P ./resources/ https://raw.githubusercontent.com/jbshp/LongDocFACTScore/refs/heads/main/src/longdocfactscore/ldfacts.py
+wget -P ./src/longdocfactscore/ https://raw.githubusercontent.com/jbshp/LongDocFACTScore/refs/heads/main/src/longdocfactscore/ldfacts.py
 
 mkdir ./data
 python -m spacy download en_core_web_sm
